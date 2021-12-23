881295296d04cc9c395c89331a35982d4f29ab13
MonikaFu <monika.furdyna@gmail.com>
2021-12-23 14:11:04 +0100

    Work with vectors and single strings

--- R/get_colours.R
+++ R/get_colours.R
@@ -1,8 +1,8 @@
-get_colours <- function(colour_name = "red", palette = palette.1in1000.plot) {
-  check_colour_name(palette, colour_name)
+get_colours <- function(colour_names = "red", palette = palette.1in1000.plot) {
+  check_colour_name(palette, colour_names)
 
   colour_hex <- palette %>%
-      filter(.data$label == colour_name) %>%
+      filter(.data$label %in% colour_names) %>%
       pull(.data$hex)
   return(colour_hex)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,19 @@
-check_colour_name <- function(palette, colour_name) {
-  if (colour_name %in% palette$label) {
+check_colour_name <- function(palette, colour_name, env = parent.frame()) {
+  .palette = deparse_1(substitute(palette, env = env))
+  if (all(colour_name %in% palette$label)) {
     return(TRUE)
   } else {
     abort(
       c(
-        "No colour in palette`` with a given `colour_name`.",
-        x = glue("Possible colour names in the palette: {toString(palette_colours$label)}.")
+        glue("No colour in {.palette} with a given `colour_name`."),
+        x = glue("Possible colour names in the palette: {toString(palette$label)}."),
+        i = "You can change the palette by specifying the `palette` argument."
       )
     )
   }
+}
+
+# Backport `base::deparse1()` to R < 4.0.0
+deparse_1 <- function(expr, collapse = " ", width.cutoff = 500L, ...) {
+  paste(deparse(expr, width.cutoff, ...), collapse = collapse)
 }
